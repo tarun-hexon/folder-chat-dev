ea587480b46af0dde684be741f4ab538560a291a
Devarajan1 <120020691+Devarajan1@users.noreply.github.com>
2023-08-08 11:47:05 +0530

    folder chat initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
