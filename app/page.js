4881a24f647d1ce3a8f97cdbc23baa31421ff418
Tarun <tsureshkumar@hexonglobal.com>
2023-12-06 23:21:26 +0530

    add route

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import LogoW from "../public/assets/Logo-W.svg";
 import Logo from "../public/assets/Logo.svg"
 import { IoSunnyOutline } from "react-icons/io5";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
     <div className='flex font-Inter justify-center items-center w-full h-screen box-border bg-[#115E59] flex-col gap-10 text-white text-4xl'>
     <Image src={LogoW} alt='logo' className='absolute self-start align-top top-3 left-2'/>
       <span className="hover:text-[#14B8A6] duration-200">Welcome To Folder Chat Landing Page...!</span>
+      <Link href={'/signup'} className="text-sm hover:text-[#14B8A6] duration-200 hover:cursor-pointer">create an account</Link>
     </div>
     </>
   )
