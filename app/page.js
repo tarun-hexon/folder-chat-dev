81f63c74a2a0691d7d3345ba3276035bba97dfb4
Tarun <tsureshkumar@hexonglobal.com>
2023-12-10 23:19:43 +0530

    effects

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { useAtom } from 'jotai'
 import { sessionAtom } from './store';
 
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
 
@@ -11,11 +12,13 @@ export default function Home() {
   const [session, setSession] = useAtom(sessionAtom); 
   const router = useRouter() 
   
+useEffect(()=> {
   if(session){
     router.push('/chat')
   }else{
     router.push('/signup')
   }
+}, [])
 
 
   return (
