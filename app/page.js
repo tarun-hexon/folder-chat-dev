90b41f6b43b974184d45388c086ec1a937d9e7d4
Tarun <tsureshkumar@hexonglobal.com>
2023-12-10 23:16:45 +0530

    edit app

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
 'use client'
 
 import { useAtom } from 'jotai'
-import { darkModeAtom, isPostSignUpCompleteAtom, isPostNameCompleteAtom, sessionAtom, onBoardCompleteAtom } from './store';
+import { sessionAtom } from './store';
 
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useAtom(darkModeAtom);
-  const [isPostSignUpComplete, setIsPostSignUpComplete] = useAtom(isPostSignUpCompleteAtom);
-  const [isPostPassComplete] = useAtom(isPostNameCompleteAtom);
-  const [onBoarding, setOnBoarding] = useAtom(onBoardCompleteAtom);
+
 
   const [session, setSession] = useAtom(sessionAtom); 
   const router = useRouter() 
